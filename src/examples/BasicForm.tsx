22fb4a0bcfcb5ed4d90cf86b59071d0aaa4257b0
lmndovi <l.ndovi92@gmail.com>
2022-05-24 10:48:25 +0100

    Added name, address & education

--- src/examples/BasicForm.tsx
+++ src/examples/BasicForm.tsx
@@ -11,6 +11,8 @@ import {
 } from "react-angular-forms";
 import { WDropDown, WInputText } from "../TemplateContainer/InputTemplates";
 import { TemplateInputContainer } from "../TemplateContainer/TemplateContainer";
+import { SaveButton } from "../TemplateContainer/SaveButton";
+
 
 export const BasicForm: FC<any> = () => {
   const asd = useState();
@@ -40,18 +42,22 @@ export const BasicForm: FC<any> = () => {
           Validators.required,
           Validators.minLength(8),
         ]),
-        document: new FormGroup({
-          type: new FormControl(), //'document.type' || 'type'
-          number: new FormControl(),
+        address: new FormGroup({
+          street: new FormControl(), 
+          city: new FormControl(),
+          zip: new FormControl(),
         }),
-        aaaaa: new FormGroup({
-          ssssss: new FormGroup({
-            ttt: new FormGroup({
-              fffffff: new FormControl(),
-            }),
+        education: new FormGroup({
+          undergraduate: new FormGroup({
+              institution: new FormControl(), 
+              graduationYear: new FormControl(),
           }),
+          postgraduate: new FormGroup({
+              institution: new FormControl(), 
+              graduationYear: new FormControl(),
         }),
-      });
+      })
+    })
       return form;
     },
   });
@@ -71,43 +77,106 @@ export const BasicForm: FC<any> = () => {
               </TemplateInputContainer>
             </WFormControl>
           </div>
+        <WFormGroup name= "address">
           <div className="p-md-2">
-            <WFormControl name="aaaaa.ssssss.ttt.fffffff">
-              <TemplateInputContainer label="FFFFFFFF">
+            <WFormControl name="street">
+              <TemplateInputContainer label="Street">
+                <WInputText />
+              </TemplateInputContainer>
+            </WFormControl>
+          {/* </div>
+          <div className="p-md-2"> */}
+            <WFormControl name="city">
+              <TemplateInputContainer label="City">
+                <WInputText />
+              </TemplateInputContainer>
+            </WFormControl>
+          {/* </div>
+          <div className="p-md-2"> */}
+            <WFormControl name="zip">
+              <TemplateInputContainer label="Zip Code">
                 <WInputText />
               </TemplateInputContainer>
             </WFormControl>
           </div>
-          <div className="p-md-2">
-            <WFormControl name="aaaaa.ssssss.ttt.fffffff">
-              <TemplateInputContainer label="FFFFFFFF">
-                <WInputText />
-              </TemplateInputContainer>
-            </WFormControl>
-          </div>
-          <WFormGroup name="document">
+        </WFormGroup>
+
+          <WFormGroup name="education">
+            <WFormGroup name="undergraduate">
             <div className="p-md-2">
-              <WFormControl name="type">
-                <TemplateInputContainer label="DOcument Type">
+              <WFormControl name="institution">
+                <TemplateInputContainer label="University Name">
                   <WInputText />
                 </TemplateInputContainer>
               </WFormControl>
-            </div>
-            <div className="p-md-2">
-              <WFormControl name="number">
-                <TemplateInputContainer label="DOcument Number">
-                  <WDropDown
+            {/* </div>
+            <div className="p-md-2"> */}
+              <WFormControl name="graduationYear">
+                <TemplateInputContainer label="Graduation Year">
+                  {/* <WDropDown
                     options={[
                       { name: "OPt1", value: 1 },
                       { name: "OPt2", value: 2 },
                     ]}
                     optionLabel="name"
-                  />
+                  /> */}
+                  <WInputText />
                 </TemplateInputContainer>
               </WFormControl>
             </div>
+            </WFormGroup>
+
+            <WFormGroup name="postgraduate">
+            <div className="p-md-2">
+              <WFormControl name="institution">
+                <TemplateInputContainer label="University Name">
+                  <WInputText />
+                </TemplateInputContainer>
+              </WFormControl>
+            {/* </div>
+            <div className="p-md-2"> */}
+              <WFormControl name="graduationYear">
+                <TemplateInputContainer label="Graduation Year">
+                  {/* <WDropDown
+                    options={[
+                      { name: "OPt1", value: 1 },
+                      { name: "OPt2", value: 2 },
+                    ]}
+                    optionLabel="name"
+                  /> */}
+                  <WInputText />
+                </TemplateInputContainer>
+              </WFormControl>
+            </div>
+            </WFormGroup>
+
+
+
+
+            {/* <div className="p-md-2">
+              <WFormControl name="institution">
+                <TemplateInputContainer label="University Name">
+                  <WInputText />
+                </TemplateInputContainer>
+              </WFormControl>
+            {/* </div>
+            <div className="p-md-2"> */}
+              {/* <WFormControl name="graduationYear">
+                <TemplateInputContainer label="Graduation Year"> */}
+                  {/* <WDropDown
+                    options={[
+                      { name: "OPt1", value: 1 },
+                      { name: "OPt2", value: 2 },
+                    ]}
+                    optionLabel="name"
+                  /> */}
+                  {/* <WInputText />
+                </TemplateInputContainer>
+              </WFormControl>
+            </div> */} 
           </WFormGroup>
         </div>
+        <SaveButton  label="GUARDAR DOCUMENTO" />
       </WForm>
     </>
   );
